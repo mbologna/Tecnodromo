60475f4499047a687e0376adcebcbfcc62de9040
sebastiano.rota@tiscali.it <sebastiano.rota@tiscali.it@2926dfc5-8347-0410-9469-37e369ee0574>
2008-06-16 13:20:36 +0000

    Prova con disegno blocco IF e aggiunta di un'istruzione

---
+++ trunk/Linguaggi/Dimension.java
@@ -0,0 +1,57 @@
+
+public class Dimension{
+
+private double dL,dR,dH,x,y;
+
+public Dimension(double dL,double dR,double dH,double x,double y)
+ {
+this.dL=dL;
+this.dR=dR;
+this.dH=dH;
+this.x=x;
+this.y=y;
+ 
+ }
+
+public double getdL()
+{
+return this.dL;}
+
+public double getdR()
+{
+return this.dR;}
+
+public double getdH()
+{
+return this.dH;}
+
+public double getX()
+{
+return this.x;}
+
+public double getY()
+{
+return this.y;}
+
+public void setdL(double dl)
+{
+	this.dL=dl;
+}
+public void setdR(double dr)
+{
+	this.dR=dr;
+}
+public void setdH(double dh)
+{
+	this.dH=dh;
+}
+public void setx(double x)
+{
+	this.x=x;
+}
+public void sety(double y)
+{
+	this.y=y;
+}
+}//End of Dimension
+
